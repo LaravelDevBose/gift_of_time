ed69d68b2c2968eea7d9ff9b5a8e90fb7403a065
LaravelDevBose <laravel.devbose@gmail.com>
2020-06-30 21:43:02 +0600

    friends and notification page design done

--- src/app/friends/friends.page.ts
+++ src/app/friends/friends.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-friends',
-  templateUrl: './friends.page.html',
-  styleUrls: ['./friends.page.scss'],
+    selector: 'app-friends',
+    templateUrl: './friends.page.html',
+    styleUrls: ['./friends.page.scss'],
 })
 export class FriendsPage implements OnInit {
 
-  constructor() { }
+    public friends =[];
+    constructor() {
+        for (let i=0; i<20; i++){
+            this.friends.push(i);
+        }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    loadData(event: any) {
+        setTimeout(() => {
+            console.log('Done');
+
+            for (let i=0; i<20; i++){
+                this.friends.push(i);
+            }
+            event.target.complete();
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+            if (this.friends.length == 100) {
+                event.target.disabled = true;
+            }
+        }, 500);
+    }
 
 }
--- src/app/notification/notification.page.ts
+++ src/app/notification/notification.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-notification',
-  templateUrl: './notification.page.html',
-  styleUrls: ['./notification.page.scss'],
+    selector: 'app-notification',
+    templateUrl: './notification.page.html',
+    styleUrls: ['./notification.page.scss'],
 })
 export class NotificationPage implements OnInit {
+    public notifyCount =[];
+    constructor() {
+        for (let i=0; i<20; i++){
+            this.notifyCount.push(i);
+        }
+    }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    loadData(event: any) {
+        setTimeout(() => {
+            console.log('Done');
 
+            for (let i=0; i<20; i++){
+                this.notifyCount.push(i);
+            }
+            event.target.complete();
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+            if (this.notifyCount.length == 100) {
+                event.target.disabled = true;
+            }
+        }, 500);
+    }
 }
